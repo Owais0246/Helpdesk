2f0833826430861a2c091ff011c773c2519b677b
Owais0246 <30753081+Owais0246@users.noreply.github.com>
2023-08-04 17:05:43 +0530

    Delete Ubold-django_v1.0.0 directory

--- Ubold-django_v1.0.0/ubold/apps/templatetags/extra.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.simple_tag()
-def multiply(op1, op2, *args, **kwargs):
-    return round(op1 * op2, 2)
--- Ubold-django_v1.0.0/ubold/pages/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PagesConfig(AppConfig):
-    name = "ubold.pages"
-    verbose_name = _("pages")
