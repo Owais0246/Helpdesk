8f045d79859a7f363edeed664b39aaed460c8531
Owais0246 <owaiskazi24@gmail.com>
2023-09-15 01:22:25 +0530

    amc updated

--- amc/models.py
+++ amc/models.py
@@ -6,13 +6,13 @@ from user.models import User
 # Create your models here.
 
 class Amc(models.Model):
-    company = models.ForeignKey(Company,on_delete=models.PROTECT, null=True,blank=True)
-    location = models.ForeignKey(Location,on_delete=models.PROTECT, null=True,blank=True)
+    company = models.ForeignKey(Company,on_delete=models.PROTECT)
+    location = models.ForeignKey(Location,on_delete=models.PROTECT)
     # user = models.ForeignKey(User,on_delete=models.PROTECT, null=True,blank=True)    
-    product= models.ForeignKey(Product,on_delete=models.PROTECT, null=True,blank=True)
-    description = models.CharField(max_length=200,blank=True)
-    startdate = models.DateTimeField()
+    product= models.ForeignKey(Product,on_delete=models.PROTECT)
+    description = models.CharField(max_length=200)
+    start_date = models.DateField( null=True,blank=True)
     duration= models.IntegerField()
     
     def __str__(self):
-        return str(self.company) +str(self.pk)
+        return str(self.company) + " - " +str(self.pk)
