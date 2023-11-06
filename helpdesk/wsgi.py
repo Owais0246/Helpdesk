5d8c0088536395d9a37a16b5843178d07895d919
Owais0246 <owaiskazi24@gmail.com>
2023-11-06 09:36:39 +0530

    wsgi

--- helpdesk/wsgi.py
+++ helpdesk/wsgi.py
@@ -10,9 +10,6 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helpdesk.settings')
 
