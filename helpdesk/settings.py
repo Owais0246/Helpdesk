d35ddd94a61501c8db3e77e13972a992bbed15a8
Owais0246 <owaiskazi24@gmail.com>
2023-12-06 12:03:46 +0530

    db

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -91,23 +91,24 @@ WSGI_APPLICATION = 'helpdesk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'helpdesk',
-#         'USER': 'service_admin',
-#         'PASSWORD': 'helpmeout',
-#         'HOST': 'localhost',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'helpdesk',
+        'USER': 'service_admin',
+        'PASSWORD': 'helpmeout',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
